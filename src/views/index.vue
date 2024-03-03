3d786bce456a8665abe7d282fbf971d6d12a501d
一纸忘忧 <i@ikxin.com>
2024-03-03 13:35:58 +0800

    feat: test axios request login api interface

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,20 +1,46 @@
 <script setup lang="ts">
+import axios from 'axios'
 import { IconPlus } from '@arco-design/web-vue/es/icon'
+
 const { t } = useI18n()
 
-const serverData = useStorage('server-data', [])
+const serverData = useStorage<any>('server-data', [])
 
 const serverDataModal = reactive({
   visible: false,
 })
 
 const serverDataForm = ref({
-  name: '',
-  ip: '',
-  port: 10000,
-  username: '',
-  password: '',
+  name: 'Example Server',
+  ip: '192.168.1.100',
+  port: '8888',
+  username: 'username',
+  password: 'password',
 })
+
+const handleBeforeOk = async () => {
+  const { data } = await axios('/api/v1/auth/login', {
+    method: 'POST',
+    data: {
+      authMethod: 'jwt',
+      captcha: '',
+      captchaID: '',
+      ignoreCaptcha: true,
+      language: 'zh',
+      name: serverDataForm.value.username,
+      password: serverDataForm.value.password,
+    },
+  })
+  if (data.code === 200) {
+    serverData.value.push({
+      ...serverDataForm.value,
+      token: data.data.token,
+    })
+    return true
+  } else {
+    return false
+  }
+}
 </script>
 
 <template>
@@ -30,22 +56,22 @@ const serverDataForm = ref({
           </template>
         </AButton>
       </div>
-      <AModal v-model:visible="serverDataModal.visible">
+      <AModal v-model:visible="serverDataModal.visible" :title="t('title.serverInfo')" :on-before-ok="handleBeforeOk">
         <AForm :model="serverDataForm">
-          <AFormItem :label="t('common.label.name')">
-            <AInput :model-value="serverDataForm.name"></AInput>
+          <AFormItem :label="t('label.name')">
+            <AInput v-model="serverDataForm.name"></AInput>
           </AFormItem>
-          <AFormItem :label="t('common.label.ip')">
-            <AInput :model-value="serverDataForm.ip"></AInput>
+          <AFormItem :label="t('label.ip')">
+            <AInput v-model="serverDataForm.ip"></AInput>
           </AFormItem>
-          <AFormItem :label="t('common.label.port')">
-            <AInputNumber :model-value="serverDataForm.port"></AInputNumber>
+          <AFormItem :label="t('label.port')">
+            <AInput v-model="serverDataForm.port"></AInput>
           </AFormItem>
-          <AFormItem :label="t('common.label.username')">
-            <AInput :model-value="serverDataForm.username"></AInput>
+          <AFormItem :label="t('label.username')">
+            <AInput v-model="serverDataForm.username"></AInput>
           </AFormItem>
-          <AFormItem :label="t('common.label.password')">
-            <AInput :model-value="serverDataForm.password"></AInput>
+          <AFormItem :label="t('label.password')">
+            <AInput v-model="serverDataForm.password"></AInput>
           </AFormItem>
         </AForm>
       </AModal>
@@ -53,8 +79,8 @@ const serverDataForm = ref({
   </ALayoutHeader>
   <AScrollbar class="max-h-[calc(100vh-4rem)] overflow-auto">
     <div class="mx-auto flex flex-col gap-4 min-h-full max-w-6xl my-4 px-4">
-      <AAlert :title="t('common.alert.notAddedServerTitle')">
-        {{ t('common.alert.notAddedServer') }}
+      <AAlert :title="t('title.prompt')">
+        {{ t('alert.notAddedServer') }}
       </AAlert>
       <div v-for="i in serverData" :key="i" class="h-32 w-full rounded border border-gray-200"></div>
     </div>
