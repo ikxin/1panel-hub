59a11862ab0351952093fe8d228aee569c2e7dbb
一纸忘忧 <i@ikxin.com>
2024-03-05 21:05:42 +0800

    fix: the icon of unocss does not take effect in the ts file

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig, presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss'
 
 export default defineConfig({
+  content: {
+    pipeline: {
+      include: [/\.(vue|svelte|[jt]s||[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/],
+    },
+  },
   presets: [presetAttributify(), presetUno(), presetIcons()],
   transformers: [transformerDirectives()],
 })
